92f41b64bde1d55e2fd4408f69b84747b32e180e
APT64 <alexandrsinowalov@gmail.com>
2026-01-11 21:58:28 +0300

    dependecy fix & meta-version update

--- modules/EtHu/scripts/banner.py
+++ modules/EtHu/scripts/banner.py
@@ -1,16 +1,8 @@
 import eternalhush
 
 def main(argv):
-    eternalhush.ui.Echo("           __________                                 ", eternalhush.ECHO_GOOD)
-    eternalhush.ui.Echo("         .'----------`.                              ", eternalhush.ECHO_GOOD)
-    eternalhush.ui.Echo("         | .--------. |                             ", eternalhush.ECHO_GOOD)
-    eternalhush.ui.Echo("         | |########| |       __________              ", eternalhush.ECHO_GOOD)
-    eternalhush.ui.Echo("         | |########| |      /__________\             ", eternalhush.ECHO_GOOD)
-    eternalhush.ui.Echo(".--------| `--------' |------|    --=-- |-------------.", eternalhush.ECHO_GOOD)
-    eternalhush.ui.Echo("|        `----,-.-----'      |o ======  |             | ", eternalhush.ECHO_GOOD)
-    eternalhush.ui.Echo("|       ______|_|_______     |__________|             | ", eternalhush.ECHO_GOOD)
-    eternalhush.ui.Echo("|      /  %%%%%%%%%%%%  \                             | ", eternalhush.ECHO_GOOD)
-    eternalhush.ui.Echo("|     /  %%%%%%%%%%%%%%  \                            | ", eternalhush.ECHO_GOOD)
-    eternalhush.ui.Echo("|     ^^^^^^^^^^^^^^^^^^^^                            | ", eternalhush.ECHO_GOOD)
-    eternalhush.ui.Echo("+-----------------------------------------------------+", eternalhush.ECHO_GOOD)
-    eternalhush.ui.Echo("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ", eternalhush.ECHO_GOOD)
+    eternalhush.ui.Echo("##############################################################", eternalhush.ECHO_GOOD)
+    eternalhush.ui.Echo("#                    ETERNALHUSH FRAMEWORK                   #", eternalhush.ECHO_GOOD)
+    eternalhush.ui.Echo("#                       VERSION 2.0.0.0	              #", eternalhush.ECHO_GOOD)
+    eternalhush.ui.Echo("#                Author: https://github.com/APT64            #", eternalhush.ECHO_GOOD)
+    eternalhush.ui.Echo("##############################################################", eternalhush.ECHO_GOOD)
